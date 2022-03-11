d0a1d7cbbb56863c5c402e85157afe22ab6838fd
Erik Polzin <eriktpol@gmail.com>
2022-03-11 07:10:09 +0200

    Fall back to default bg is blurred image not found

--- udp_chat/gui_client/main_window.py
+++ udp_chat/gui_client/main_window.py
@@ -41,7 +41,10 @@ class MainWindow(QMainWindow):
         logging.info(f"Created GUI client at {server_addr[0]}:{server_addr[1]}")
         self.settings = QSettings("uct.ac.za", "udpchat")
         self.bg_pixmap = QPixmap(self.settings.value("wallpaper", ":/background.jpg", type=str))
-        self.bg_pixmap_alt = QPixmap(self.settings.value("wallpaper_alt", ":/background-blurred.jpg", type=str))
+        self.bg_pixmap_alt = QPixmap()
+        # If the alternative pixmap couldn't be loaded, use the default one
+        if not self.bg_pixmap_alt.load(self.settings.value("wallpaper_alt", ":/background-blurred.jpg", type=str)):
+            self.bg_pixmap_alt = self.bg_pixmap
         self.message_backlog: Queue[UDPMessage] = Queue()
         self.first_connect = True
         self.username = None
